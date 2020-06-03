32507e53c8e78cf0dca946d540e2321d8a033124
spadesk <spadesk1991@gmail.com>
2020-06-03 23:30:56 +0800

    feat: 增加http调用

---
+++ client/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/spadesk1991/go-micro-demo/lib"
+
+	"github.com/micro/go-micro/client/selector"
+
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/web"
+	"github.com/micro/go-plugins/registry/consul"
+)
+
+func main() {
+	reg := consul.NewRegistry(registry.Addrs("localhost:8500"))
+	getServicd, err := reg.GetService("prodService")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//myhttp.NewClient(client.)
+	var s selector.Selector
+	// 定义选择器类型
+	s = selector.NewSelector(
+		selector.Registry(reg),
+		selector.SetStrategy(selector.RoundRobin),
+	)
+	router := gin.Default()
+
+	type resModel struct {
+		ID   int
+		Name string
+	}
+	type res struct {
+		Data []resModel `json:"data"`
+	}
+
+	router.GET("/news", func(ctx *gin.Context) {
+		next := selector.RoundRobin(getServicd)
+		node, err := next()
+		if err != nil {
+			log.Fatal(err)
+		}
+		r := new(res)
+		buff, err := lib.Call("GET", "http://"+node.Address+"/news", "", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.Unmarshal(buff, r)
+		ctx.JSON(http.StatusOK, gin.H{"datas": r})
+	})
+
+	router.GET("/v2/news", func(ctx *gin.Context) {
+		lib.Call2(s, ctx)
+	})
+
+	srv := web.NewService(
+		web.Name("web_service"),
+		web.Address(":8002"),
+		web.Registry(reg),
+		web.Handler(router),
+	)
+	srv.Run()
+}
---
+++ lib/request.go
@@ -0,0 +1,44 @@
+package lib
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/micro/go-micro/client"
+
+	"github.com/micro/go-micro/client/selector"
+	myhttp "github.com/micro/go-plugins/client/http"
+)
+
+func Call(method, url, params string, body io.Reader) (buff []byte, err error) {
+	request, err := http.NewRequest(method, url+params, body)
+	if err != nil {
+		return
+	}
+	client := http.DefaultClient
+	r, err := client.Do(request)
+	defer r.Body.Close()
+	if err != nil {
+		return
+	}
+	buff, _ = ioutil.ReadAll(r.Body)
+	return
+}
+
+func Call2(s selector.Selector, ctx context.Context) {
+	cli := myhttp.NewClient(
+		client.Selector(s),
+		client.ContentType("application/json"),
+	)
+	req := cli.NewRequest("prodService", "/news", map[string]interface{}{})
+	var res map[string]interface{}
+	err := cli.Call(ctx, req, &res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res["data"])
+}
